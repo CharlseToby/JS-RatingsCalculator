4fcd506fd1f2126b2488c58bbf43d013a72e449f
Toochukwu Okolie <charlestoochukwu5@gmail.com>
2021-11-17 02:10:51 +0100

    project completed

--- js/ratings.js
+++ js/ratings.js
@@ -1,9 +1,5 @@
 function collect_ratings() {
-    const ratings = {
-        'sum': 0,
-        'count': 0,
-        'average': 0,
-    };
+    const ratings = {'count': 0, 'sum': 0, 'average': 0,};
     let rating = 0;
 
     const elements = document.querySelectorAll(".rating");
